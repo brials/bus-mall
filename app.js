cde3dab48382df1619b071679fd6ef94ef921fef
brials <brials008@hotmail.com>
2016-12-12 15:54:11 -0800

    Added image click re render functionality and count.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var objArray =[];
 var imagePlace = document.getElementById('images');
 var chartPlace = document.getElementById('chart');
+var buttonPlace = document.getElementById('button');
+var globalCount = 0;
 
 function Image(url, id, count, viewed){
   this.id = id;
@@ -46,7 +48,33 @@ function render(el, content, place, newId, image){
   console.log(place);
   place.appendChild(tempEl);
 }
-// Function for handling images
+
+// Function to search through array
+function lookInArray(id){
+  for(var i = 0; i < objArray.length; i++){
+    if(objArray[i].id === id){
+      return i;
+    }
+  }
+}
+// Function for handling image clicks
+function handleImageClick(event){
+  event.preventDefault();
+  console.log(event.target.id);
+  var name = event.target.id;
+  if(name === 'images'){
+    return alert('Please click an image');
+  }
+  var index = lookInArray(name);
+  console.log(index);
+  objArray[index].count += 1;
+  renderImages();
+  globalCount += 1;
+  if(globalCount === 5){
+    imagePlace.innerHTML = '';
+    render('button', 'Click Here', 'button')
+  }
+}
 
 // Function for selecting 3 non duplicate images.
 function threeImageRandomizer(){
@@ -56,9 +84,9 @@ function threeImageRandomizer(){
     var temp = randomImage();
     if(!objArray[temp].used){
       objArray[temp].used = true;
-      console.log(objArray[temp].used);
       array.push(temp);
       count +=1;
+      objArray[temp].viewed += 1;
     }
   }
   for(var i = 0; i < objArray.length; i++){
@@ -90,3 +118,6 @@ function renderImages(){
 if(objArray.length === 0){
   initialArray();
 }
+renderImages();
+imagePlace.addEventListener('click', handleImageClick);
+buttonPlace.addEventListener('click', handleButtonClick);
